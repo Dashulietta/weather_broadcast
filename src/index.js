7f9f333d9c8548498ce5aeaf8d9c6747dcadafbc
Dashulietta <108624114+Dashulietta@users.noreply.github.com>
2022-08-04 11:37:33 +0300

    Integrated  api for weather forecast

--- src/index.js
+++ src/index.js
@@ -44,8 +44,55 @@ if (minutes < 10) {
 let time = document.querySelector("#time");
 time.innerHTML = `${hours}:${minutes}`;
 
+
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHTML = `<div class="row">`;
+ 
+  forecast.forEach(function(forecastDay, index) {
+    if (index < 6) {
+ forecastHTML = forecastHTML + ` 
+ 
+
+  <div class = "col-2">
+   <div class="forecast-date">
+    ${formatDay(forecastDay.dt)}
+
+    </div>
+  <img src= "http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+  alt="" 
+  width="42"/>
+  <div class ="weather-forecast-temp">
+    <span class="weather-forecast-temp-max"> ${Math.round(forecastDay.temp.max)}°</span>
+  <span class="weather-forecast-temp-min">${Math.round(forecastDay.temp.min)}°</span>
+</div>
+</div>
+`;}
+});
+
+forecastHTML = forecastHTML + `</div>`;
+forecastElement.innerHTML = forecastHTML; 
+}
+
 ///
 let celsiusTemperature = null;
+
+function getForecast(coordinates) {
+  let apiKey = "6f1066b52fb74e7c4c41b08e58f115f4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
   celsiusTemperature = response.data.main.temp;
@@ -59,7 +106,9 @@ function displayWeather(response) {
   );
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#emoji").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
- 
+ getForecast(response.data.coord);
+
+
 }
 
 
